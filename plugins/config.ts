0ac6621d160844e566b2f033f8f8363843695f55
Craig Morris <craig.michael.morris@gmail.com>
2021-12-17 17:46:37 +0000

    config

--- plugins/config.ts
+++ plugins/config.ts
@@ -1,8 +1,8 @@
 import { PlanetLevel } from "@darkforest_eth/types";
 
 export const config = {
-  MIN_LEVEL_PLANET: PlanetLevel.THREE,
+  MIN_LEVEL_PLANET: PlanetLevel.ONE,
   MAX_LEVEL_PLANET: PlanetLevel.NINE,
-  MIN_LEVEL_ASTEROID: PlanetLevel.THREE,
+  MIN_LEVEL_ASTEROID: PlanetLevel.ONE,
   MAX_LEVEL_ASTEROID: PlanetLevel.NINE,
 }
